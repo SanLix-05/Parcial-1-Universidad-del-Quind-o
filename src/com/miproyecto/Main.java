20d1829f501aa699e3c0aa65be889b531ff02b4c
SanLix-05 <sholguinbermudez@gmail.com>
2026-09-24 21:48:14 -0500

    Se implemento condicionales de validacion para saber si el cliente ya tiene un contrato

--- src/com/miproyecto/Main.java
+++ src/com/miproyecto/Main.java
@@ -13,13 +13,22 @@ public class Main {
             opcion = respuesta;
             switch (opcion){
                 case 1:
-                    respuesta = Integer.parseInt(JOptionPane.showInputDialog("DEVPLUS\n" + "1. Registrarse\n" + "2.Crear proyecto \n"
-                    + "3. Servicios adicionales\n" + " 4.salir"));
-                    if (respuesta == 1){
-                        Cliente.registroCliente();
-                    } else if (respuesta == 2) {
-                        
-                    }
+                    do {
+                        respuesta = Integer.parseInt(JOptionPane.showInputDialog("DEVPLUS\n" + "1. Registrarse\n" + "2.Crear proyecto \n"
+                                + "3. Servicios adicionales\n" + " 4.salir"));
+                        if (respuesta == 1){
+                          Cliente.registroCliente();
+                        } else if (respuesta == 2) {
+                            String nit = JOptionPane.showInputDialog("Ingrese el nit sin puntos ni comas de la empresa");
+                            if(!Proyecto.getProyectosClientes().containsKey(nit)){
+                                JOptionPane.showMessageDialog(null,"Restrese primero");
+                            }
+                            else {
+                                Cliente.crearProyecto();
+                            }
+                        }
+
+                    }while ();
 
             }
 
